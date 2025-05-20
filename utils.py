c07b87fc45c6848caab5f2c5baf011af2679c03a
poornesh0604 <poorneshofficial06@gmail.com>
2025-05-20 19:49:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+def get_color_name(R, G, B, df):
+    minimum = float('inf')
+    cname = "Unknown"
+    for i in range(len(df)):
+        d = abs(R - int(df.loc[i, "R"])) + abs(G - int(df.loc[i, "G"])) + abs(B - int(df.loc[i, "B"]))
+        if d < minimum:
+            minimum = d
+            cname = df.loc[i, "color_name"]
+    return cname
 
